8685bb0d2937bfa44de12961f06ca2dc351a4b72
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2022-12-04 19:21:48 +0800

    2022-12-03

--- 2022-12-03.js
+++ 2022-12-03.js
@@ -1,9 +1,12 @@
-/* 1. 两数之和 */
+/* 1796. 字符串中第二大的数字 */
 
-const twoSum = (nums, target) => {
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[j] === target - nums[i]) return [i, j];
-        }
+const secondHighest = s => {
+    const numbers = new Set();
+    for (const char of s) {
+        if (char >= "0" && char <= "9") numbers.add(parseInt(char));
     }
+    const sortedArray = Array.from(numbers).sort().reverse();
+    return sortedArray[1] === undefined ? -1 : sortedArray[1];
 };
+
+console.log(secondHighest("ck077"));

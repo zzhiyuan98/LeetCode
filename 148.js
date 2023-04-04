66f13b03ad2d6c4586cd50dd58e1395b41d4d3a1
Zhiyuan Zhang <zhangzhiyuan@datarc.cn>
2023-04-04 13:47:03 +0800

    148 temp

---
+++ 148.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const sortList = head => {
+
+  const mergeSort = (start, end) =>{
+    let slow = start;
+    let fast = start;
+
+  };
+
+
+
+};

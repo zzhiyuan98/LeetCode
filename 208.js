13d3be61085790fed5de2e148463cfd79e6e90e0
Zhiyuan Zhang <zhangzhiyuan@datarc.cn>
2023-04-13 13:59:35 +0800

    208 temp

---
+++ 208.js
@@ -0,0 +1,32 @@
+function TreeNode(val) {
+  this.val = val;
+  this.children = [];
+}
+
+var Trie = function() {
+
+};
+
+/**
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+
+};
+
+/**
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+
+};

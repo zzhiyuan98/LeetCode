5cdd010189c7557aee2b2f3de6738d3d1c520293
Zhiyuan Zhang <zhangzhiyuan@datarc.cn>
2023-06-13 19:34:54 +0800

    48 rotate temp

--- 48-100.js
+++ 48-100.js
@@ -1,14 +1,12 @@
+/*
+48. 旋转图像
+
+给定一个 n * n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+
+你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ */
+
 const rotate = matrix => {
-  /*
-    1 2 3
-    4 5 6
-    7 8 9
-
-    7 4 1
-    8 5 2
-    9 6 3
-  */
-
   /*
     1  2  3  4
     5  6  7  8
@@ -21,9 +19,17 @@ const rotate = matrix => {
     16 12 8  4
    */
   const n = matrix.length;
-  const swap = (i, j) => {
-    const temp = matrix[i][j];
-  };
+  let depth = 0;
+  while (depth <= n / 2) {
+    const len = n - 2 * depth;
+    for (let i = 0; i < len - 1; i++) {
+      const temp = matrix[depth][depth + i];
+
+
+    }
+
+  }
+
 };
 
 // expected output: [[7,4,1],[8,5,2],[9,6,3]]

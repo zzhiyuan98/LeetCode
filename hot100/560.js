cdd2cb6be486d27cbea65ca0bff2b2c3c238792b
Zhiyuan Zhang <zhangzhiyuan@datarc.cn>
2023-06-21 15:02:52 +0800

    632 temp & 560 optimization

--- hot100/560.js
+++ hot100/560.js
@@ -19,6 +19,26 @@ const subarraySum = (nums, k) => {
  return count;
 };
 
+/*
+优化：不关心哪两项的前缀和之差等于 k，只关心等于 k 的前缀和之差出现了几次
+*/
+
+const subarraySumNew = (nums, k) => {
+  const map = new Map();
+  map.set(0, 1);
+  let count = 0;
+  let preSum = 0;
+  for (const num of nums) {
+    preSum += num;
+    if (map.has(preSum - k)) {
+      count += map.get(preSum - k);
+    }
+    const temp = map.has(preSum) ? map.get(preSum) + 1 : 1;
+    map.set(preSum, temp);
+  }
+  return count;
+};
+
 const nums = [1,1,1];
 const k = 2;
-console.log(subarraySum(nums, k));
+console.log(subarraySum(nums, k) === subarraySumNew(nums, k));

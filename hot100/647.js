8489036afddf09653fa1ac9b2cd7ada61553de31
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-06-27 14:53:30 +0800

    647

--- hot100/647.js
+++ hot100/647.js
@@ -11,11 +11,21 @@
 */
 
 const countSubstrings = s => {
-
-
-
+  const n = s.length;
+  // dp[i][j] 表示 s 在 [i, j] 是否是一个回文子串
+  const dp = Array.from(Array(n), () => new Array(n));
+  let count = 0;
+  for (let j = 0; j < n; j++) {
+    for (let i = 0; i <= j; i++) {
+      if (s[i] === s[j] && (j - i + 1 <= 2 || dp[i + 1][j - 1])) {
+        dp[i][j] = true;
+        count++;
+      }
+    }
+  }
+  return count;
 };
 
-// expected output: 3
-const s = "abc";
+// expected output: 6
+const s = "aaa";
 console.log(countSubstrings(s));

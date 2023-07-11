db3518eb30d16ab49152c795989f9e7e04558533
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-11 16:01:40 +0800

    get maximum volumn

--- 11.js
+++ 11.js
@@ -1,16 +1,32 @@
-var minArray = function(numbers) {
-  let left = 0;
-  let right = numbers.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    const num = numbers[mid];
-    if (num < numbers[right]) right = mid;
-    else if (num > numbers[right]) left = mid + 1;
-    else if (num === numbers[right]) right--;
+/*
+11. 盛最多水的容器
 
+给定一个长度为 n 的整数数组 height。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i])。
+
+找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+返回容器可以储存的最大水量。
+
+说明：你不能倾斜容器。
+ */
+
+const maxArea = height => {
+  let left = 0;
+  let right = height.length - 1;
+  let max = 0;
+  while (left < right) {
+    const area = Math.min(height[left], height[right]) * (right - left);
+    max = Math.max(max, area);
+    if (height[left] < height[right]) {
+      left++;
+    }
+    else {
+      right--;
+    }
   }
-  return numbers[left];
+  return max;
 };
 
-const numbers = [3,4,5,1,2];
-console.log(minArray(numbers));
+// 49
+const height = [1,8,6,2,5,4,8,3,7];
+console.log(maxArea(height));

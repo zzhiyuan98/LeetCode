61f0758be6c041c4061d57bc34f7ed9452cc12dd
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-11 12:27:49 +0800

    longest palindrome

--- hot100/5.js
+++ hot100/5.js
@@ -1,31 +1,30 @@
-var longestPalindrome = function(s) {
-  const len = s.length;
+/*
+5. 最长回文子串
+
+给你一个字符串 s，找到 s 中最长的回文子串。
+
+如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+ */
+
+const longestPalindrome = s => {
+  const n = s.length;
   // dp[i][j] 表示 s.substring(i, j + 1) 是否回文
-  const dp = Array.from(Array(len), () => new Array(len));
-  // 初始化，单字母字符串自身回文
-  for (let i = 0; i < len; i++) {
-    dp[i][i] = true;
-  }
-  let maxLength = 1;
-  let start = 0;
-  for (let curLength = 2; curLength <= len; curLength++) {
-    // 枚举左边界
-    for (let i = 0; i < len; i++) {
-      // 计算右边界
-      const j = i + curLength - 1;
-      if (j >= len) break;
-      if (s[i] !== s[j]) dp[i][j] = false;
-      else {
-        if (curLength <= 3) dp[i][j] = true;
-        else dp[i][j] = dp[i + 1][j - 1];
+  const dp = Array.from(Array(n), () => new Array(n).fill(false));
+  let max = 0;
+  let ans = "";
+  for (let len = 1; len <= s.length; len++) {
+    for (let i = 0; i < n - len + 1; i++) {
+      const j = i + len - 1;
+      if (s[i] === s[j]) {
+        dp[i][j] = len <= 3 ? true : dp[i + 1][j - 1];
       }
-      if (dp[i][j] && curLength > maxLength) {
-        maxLength = curLength;
-        start = i;
+      if (dp[i][j] && len > max) {
+        max = len;
+        ans = s.substring(i, j + 1);
       }
     }
   }
-  return s.substring(start, start + maxLength);
+  return ans;
 };
 
 const s = "babad";

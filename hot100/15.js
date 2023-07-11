aaa82e10ce572df3c5a1bdd7ff1571b44564876f
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-11 16:22:27 +0800

    refactor: three sum

--- hot100/15.js
+++ hot100/15.js
@@ -1,36 +1,51 @@
+/*
+15. 三数之和
+
+给你一个整数数组 nums，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k，同时还满足 nums[i] + nums[j] + nums[k] == 0。
+
+请你返回所有和为 0 且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+ */
+
+// 排序 + 双指针
 const threeSum = nums => {
-  nums.sort();
-  const twoSum = (target, start, end = nums.length - 1) => {
-    const set = new Set();
-    const recorded = new Set();
-    const res = [];
-    for (let i = start; i <= end; i++) {
-      const num = nums[i];
-      if (set.has(target - num)) {
-        if (!recorded.has(num) && !recorded.has(target - num)) res.push([num, target - num]);
-        recorded.add(num);
-        recorded.add(target - num);
-      }
-      set.add(num);
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  const ans = [];
+  for (let i = 0; i < n ; i++) {
+    if (nums[i] > 0) {
+      return ans;
     }
-    return res;
-  };
-  const res = [];
-  const recorded = new Set();
-  for (let i = 0; i < nums.length - 2; i++) {
-    const num = nums[i];
-    if (recorded.has(num)) continue;
-    const two = twoSum(-num, i + 1);
-    if (two.length !== 0) {
-      for (const ele of two) {
-        ele.push(num);
-        res.push(ele);
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let left = i + 1;
+    let right = n - 1;
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        ans.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) {
+          left++;
+        }
+        while (left < right && nums[right] === nums[right - 1]) {
+          right--;
+        }
+        left++;
+        right--;
       }
-      recorded.add(num);
+      else if (sum > 0) {
+        right--;
+      }
+      else {
+        left++;
+      }
     }
   }
-  return res;
+  return ans;
 };
 
+// [[-1,-1,2], [-1,0,1]]
 const nums = [-1,0,1,2,-1,-4];
 console.log(threeSum(nums));

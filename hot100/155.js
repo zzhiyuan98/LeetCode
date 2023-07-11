409fa2c8c0eb6f61352789c728a23516fd743ffa
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-11 19:38:20 +0800

    docs: min stack

--- hot100/155.js
+++ hot100/155.js
@@ -1,3 +1,17 @@
+/*
+155. 最小栈
+
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+实现 MinStack 类:
+
+MinStack() 初始化堆栈对象。
+void push(int val) 将元素val推入堆栈。
+void pop() 删除堆栈顶部的元素。
+int top() 获取堆栈顶部的元素。
+int getMin() 获取堆栈中的最小元素。
+ */
+
 function MinStack() {
   this.stack = [];
   this.minStack = [];

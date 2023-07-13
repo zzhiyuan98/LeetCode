e9cb57a6267790f8ae230abf567dcbc2dad5b5c2
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-13 17:17:18 +0800

    docs: add info

--- hot100/142.js
+++ hot100/142.js
@@ -1,3 +1,13 @@
+/*
+142. 环形链表 II
+
+给定一个链表的头节点 head，返回链表开始入环的第一个节点。如果链表无环，则返回 null。
+
+如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+不允许修改链表。
+ */
+
 function ListNode(val) {
   this.val = val;
   this.next = null;
@@ -8,8 +18,10 @@ const detectCycle = head => {
   const set = new Set();
   let curr = head;
   while (curr) {
-    if (set.has(curr)) return curr;
-    else set.add(curr);
+    if (set.has(curr)) {
+      return curr;
+    }
+    set.add(curr);
     curr = curr.next;
   }
   return null;

9886ec9aa8c72e9ceda2e8ff1300666db62263de
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-25 19:52:13 +0800

    docs: problem statement

---
+++ 0725.js
@@ -0,0 +1,9 @@
+const findMedianSortedArrays = (nums1, nums2) => {
+  const m = nums1.length;
+  const n = nums2.length;
+
+};
+
+const nums1 = [1,3];
+const nums2 = [2];
+console.log(findMedianSortedArrays(nums1, nums2));

a09ee51ce5c70b368a8c16f9edf3dd83bfbba993
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-26 15:01:06 +0800

    refactor: min path sum

--- hot100/64.js
+++ hot100/64.js
@@ -1,4 +1,32 @@
-const minPathSum = grid => {
+/*
+64. 最小路径和
+
+给定一个包含非负整数的 m x n 网格 grid，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+
+说明：每次只能向下或者向右移动一步。
+ */
+
+var minPathSum = function(grid) {
+  const m = grid.length;
+  const n = grid[0].length;
+  // dp[i][j] 表示从左上角到 (i, j) 的最小路径和
+  const dp = Array.from(Array(m), () => new Array(n));
+  dp[0][0] = grid[0][0];
+  for (let j = 1; j < n; j++) {
+    dp[0][j] = dp[0][j - 1] + grid[0][j];
+  }
+  for (let i = 1; i < m; i++) {
+    dp[i][0] = dp[i - 1][0] + grid[i][0];
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+    }
+  }
+  return dp[m - 1][n - 1];
+};
+
+const minPathSum1 = grid => {
   const m = grid.length;
   const n = grid[0].length;
   const cachedSum = Array.from(Array(m), () => new Array(n));

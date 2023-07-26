7ea1df433be4ba0089e9026c89ac427f1fd1dbe1
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-26 16:30:06 +0800

    refactor: exists

--- hot100/79.js
+++ hot100/79.js
@@ -9,36 +9,31 @@
 var exist = function(board, word) {
   const m = board.length;
   const n = board[0].length;
-  const wordLength = word.length;
   const visited = Array.from(Array(m), () => new Array(n));
-  const recur = (i, j, charIndex) => {
-    if (i < 0 || j < 0 || i >= m || j >= n || visited[i][j]) return false;
-    if (board[i][j] === word[charIndex]) {
-      if (charIndex === wordLength - 1) return true;
-      visited[i][j] = true;
-      const next = charIndex + 1;
-      if (recur(i - 1, j, next) || recur(i + 1, j, next) || recur(i, j - 1, next) || recur(i, j + 1, next)) {
-        return true;
-      }
-      else {
-        visited[i][j] = false;
-        return false;
+  const directions = [[0,1],[0,-1],[-1,0],[1,0]];
+  const isLegal = (i, j) => i >= 0 && i < m && j >= 0 && j < n;
+  const dfs = (i, j, index) => {
+    if (board[i][j] !== word[index]) return false;
+    if (index === word.length - 1) return true;
+
+    for (const d of directions) {
+      const x = i + d[0];
+      const y = j + d[1];
+      if (isLegal(x, y) && !visited[x][y]) {
+        visited[x][y] = true;
+        if (dfs(x, y, index + 1)) return true;
+        visited[x][y] = false;
       }
     }
     return false;
   };
+
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      if (recur(i, j, 0)) return true;
+      visited[i][j] = true;
+      if (dfs(i, j, 0)) return true;
+      visited[i][j] = false;
     }
   }
   return false;
 };
-
-/*
-  [
-    [A, B, C, E],
-    [S, F, E, S],
-    [A, D, E, E],
-  ]
- */

c05566e2a515bc84d33e1a471b8127934e35f830
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-26 14:54:07 +0800

    refactor: unique paths

--- hot100/62.js
+++ hot100/62.js
@@ -1,4 +1,34 @@
-const uniquePaths = (m, n) => {
+/*
+62. 不同路径
+
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start”）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+
+问总共有多少条不同的路径？
+ */
+
+// 动态规划
+var uniquePaths = function(m, n) {
+  // dp[i][j] 表示从起点到达 (i, j) 有多少条不同的路径
+  const dp = Array.from(Array(m), () => new Array(n));
+  dp[0][0] = 1;
+  for (let j = 1; j < n; j++) {
+    dp[0][j] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
+};
+
+// 排列组合
+const uniquePaths1 = (m, n) => {
   /*
     对于 3 * 7 的网格，从左上角到左下角
     一共需要往右 2 次，往下 6 次

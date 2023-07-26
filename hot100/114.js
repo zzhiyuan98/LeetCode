f2a885e3f6778ed83b922723f5bf38a0754bbcf5
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-26 17:48:55 +0800

    flatten

--- hot100/114.js
+++ hot100/114.js
@@ -12,8 +12,26 @@ function TreeNode(val, left, right) {
   this.right = (right===undefined ? null : right)
 }
 
-// 前序遍历形成单链表
+// 一边遍历一边展开
 const flatten = root => {
+  if (!root) return;
+  const stack = [];
+  stack.push(root);
+  let prev = null;
+  while (stack.length) {
+    const curr = stack.pop();
+    if (prev !== null) {
+      prev.left = null;
+      prev.right = curr;
+    }
+    if (curr.right) stack.push(curr.right);
+    if (curr.left) stack.push(curr.left);
+    prev = curr;
+  }
+};
+
+// 前序遍历形成单链表
+const flatten1 = root => {
   const preorder = [];
   const dfs = root => {
     if (!root) return;

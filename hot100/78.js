01df65e6e65d87f59448ed932dec1b7be7657560
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-26 16:12:11 +0800

    subsets

--- hot100/78.js
+++ hot100/78.js
@@ -1,6 +1,29 @@
-const subsets = nums => {
+/*
+78. 子集
+
+给你一个整数数组 nums，数组中的元素互不相同。返回该数组所有可能的子集（幂集）。
+
+解集不能包含重复的子集。你可以按任意顺序返回解集。
+ */
+
+const subsets = function(nums) {
   const ans = [];
-  const n = nums.length;
+  const dfs = (nums, output, n, start) => {
+    if (output.length === n) {
+      ans.push(output);
+    }
+    for (let i = start; i < nums.length; i++) {
+      dfs(nums.slice(0, i).concat(nums.slice(i + 1)), output.concat(nums[i]), n, i);
+    }
+  };
+  for (let i = 0; i <= nums.length; i++) {
+    dfs(nums, [], i, 0);
+  }
+  return ans;
+};
+
+const subsets1 = nums => {
+  const ans = [];
   const dfs = (output, start) => {
     ans.push(output);
     if (output.length === nums.length) return;

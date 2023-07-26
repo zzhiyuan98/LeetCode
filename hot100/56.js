7a5460a4b203128a60392d18d5cb32ac681f1468
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-07-26 14:47:25 +0800

    docs: merge intervals

--- hot100/56.js
+++ hot100/56.js
@@ -1,4 +1,9 @@
-/* 合并所有重叠区间，返回不重叠的区间 */
+/*
+56. 合并区间
+
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [start_i, end_i]。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+ */
+
 const merge = intervals => {
   intervals.sort((a, b) => a[0] - b[0]);
   const ans = [];

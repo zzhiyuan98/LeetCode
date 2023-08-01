fe61a6ce2e51754471d8945889efdcb407cc649d
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-08-01 14:28:58 +0800

    refactor: find Kth largest

--- hot100/215.js
+++ hot100/215.js
@@ -1,4 +1,14 @@
-import PQ from "./utils/PQ.js";
+/*
+215. 数组中的第K个最大元素
+
+给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+
+请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ */
+
+import PQ from "../utils/PQ.js";
 
 const partition = (nums, low, high) => {
   const pivot = nums[low];

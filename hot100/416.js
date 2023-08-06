e5d802360f94e47a85fb35e8c1586295759a994a
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-08-06 17:55:10 +0800

    refactor: can partition

--- hot100/416.js
+++ hot100/416.js
@@ -6,29 +6,23 @@
 
 const canPartition = nums => {
   const n = nums.length;
-  if (n < 2) return false;
   const sum = nums.reduce((prev, curr) => prev + curr, 0);
-  if (sum % 2 !== 0) return false;
-  const target = sum / 2
-  if (Math.max(...nums) > target) return false;
-
-  // dp[i][j] 表示从数组的 [0,i] 内取若干个数（可以是 0 个），是否能使和为 j
-  const dp = Array.from(Array(n), () => new Array(target + 1).fill(false));
-  dp[0][nums[0]] = true;
-  for (let i = 0; i < n; i++) {
-    dp[i][0] = true;
+  const target = sum / 2;
+  if (!Number.isInteger(target) || nums.some(value => value > target)) {
+    return false;
   }
-  for (let i = 1; i < n; i++) {
+  // dp[i][j] 表示选取 nums 里的前 i 个数，是否可以使和为 j
+  const dp = Array.from(Array(n + 1), () => new Array(target + 1).fill(false));
+  dp[0][0] = true;
+  for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= target; j++) {
-      if (j < nums[i]) {
-        dp[i][j] = dp[i - 1][j];
-      }
-      else {
-        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+      const num = nums[i - 1];
+      if (dp[i - 1][j] || (num <= j && dp[i - 1][j - num])) {
+        dp[i][j] = true;
       }
     }
   }
-  return dp[n - 1][target];
+  return dp[n][target];
 };
 
 const nums = [1,5,11,5];

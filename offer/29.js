81d5a2c7c690e2b7beee08e6aa3b82f32b40a420
zhangzhiyuan <zhangzhiyuan@datarc.cn>
2023-08-22 18:29:18 +0800

    refactor: spiral order

--- offer/29.js
+++ offer/29.js
@@ -1,38 +1,35 @@
 const spiralOrder = function(matrix) {
   const m = matrix.length;
-  if (m === 0) {
-    return [];
-  }
+  if (m === 0) return [];
   const n = matrix[0].length;
-  const ans = [];
 
+  const ans = [];
   let top = 0;
   let bottom = m - 1;
   let left = 0;
   let right = n - 1;
 
-  const check = () => ans.length === m * n;
-
-  while (!check()) {
+  while (true) {
     for (let i = left; i <= right; i++) {
       ans.push(matrix[top][i]);
     }
-    if (check()) break;
     top++;
+    if (top > bottom) break;
     for (let i = top; i <= bottom; i++) {
       ans.push(matrix[i][right]);
     }
-    if (check()) break;
     right--;
+    if (left > right) break;
     for (let i = right; i >= left; i--) {
       ans.push(matrix[bottom][i]);
     }
-    if (check()) break;
     bottom--;
+    if (top > bottom) break;
     for (let i = bottom; i >= top; i--) {
       ans.push(matrix[i][left]);
     }
     left++;
+    if (left > right) break;
   }
   return ans;
 };

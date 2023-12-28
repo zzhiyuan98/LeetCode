435eb596f72c4dd6a17fd20a0626e7c68c4f92b0
zzhiyuan98 <zzhiyuan@umich.edu>
2023-12-28 13:29:09 +0800

    initialize 2645-addMinimum

---
+++ JavaScript/2645-addMinimum.js
@@ -0,0 +1,15 @@
+/*
+2645. 构造有效字符串的最少插入数
+
+给你一个字符串 word ，你可以向其中任何位置插入 "a"、"b" 或 "c" 任意次，返回使 word 有效 需要插入的最少字母数。
+
+如果字符串可以由 "abc" 串联多次得到，则认为该字符串 有效 。
+ */
+
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var addMinimum = function(word) {
+
+};

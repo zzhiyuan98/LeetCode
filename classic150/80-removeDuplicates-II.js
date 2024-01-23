c4e20c65103707b30392b5dc292e815a65369a6b
zzhiyuan98 <zzhiyuan@umich.edu>
2024-01-23 13:32:42 +0800

    classic 150 - removeDuplicates II

--- classic150/80-removeDuplicates-II.js
+++ classic150/80-removeDuplicates-II.js
@@ -4,17 +4,25 @@
  */
 var removeDuplicates = function(nums) {
   let prev = nums[0];
+  let count = 1;
   let k = 1;
   for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== prev) {
+    if (nums[i] === prev) {
+      if (count === 1) {
+        nums[k] = nums[i];
+        count++;
+        k++;
+      }
+    } else {
       nums[k] = nums[i];
       prev = nums[i];
+      count = 1;
       k++;
     }
   }
   return k;
 };
 
-const nums = [1,1,1,2,2,3];
+const nums = [1,1,1,1,1,2,3,3]
 console.log(removeDuplicates(nums));
 console.log(nums);

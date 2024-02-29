9798b6ecd2b2c7cce97f8c87c00204f3fb92c6bc
zzhiyuan98 <zzhiyuan@umich.edu>
2024-02-29 13:03:19 +0800

    classic 150: three sum

--- classic150/15-threeSum.js
+++ classic150/15-threeSum.js
@@ -4,18 +4,17 @@
  */
 var threeSum = function(nums) {
   const ans = [];
-  nums.sort();
-  console.log(nums);
+  nums.sort((a, b) => a - b);
   for (let i = 0; i < nums.length; i++) {
-    const curr = nums[i];
-    const prev = i > 0 ? nums[i - 1] : null;
-    if (curr === prev) {
+    if (nums[i] > 0) {
+      return ans;
+    }
+    if (i > 0 && nums[i] === nums[i - 1]) {
       continue;
     }
-    const twoSumResult = twoSum(nums.slice(0, i).concat(nums.slice(i + 1)), -curr);
-    console.log(curr, twoSumResult)
+    const twoSumResult = twoSum(nums.slice(i + 1), -nums[i]);
     twoSumResult.forEach(twoSumArr => {
-      ans.push([curr].concat(twoSumArr));
+      ans.push([nums[i]].concat(twoSumArr));
     })
   }
   return ans;
@@ -23,23 +22,25 @@ var threeSum = function(nums) {
 
 function twoSum(numbers, target) {
   const ans = [];
-  let i = 0;
-  let j = numbers.length - 1;
-  while (i < j) {
-    const sum = numbers[i] + numbers[j];
+  const n = numbers.length;
+  let left = 0;
+  let right = n - 1;
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
     if (sum === target) {
-      ans.push([numbers[i], numbers[j]]);
-      do {
-        i++;
-      } while (i < j && numbers[i] === numbers[i - 1]);
-      do {
-        j--;
-      } while (i < j && numbers[j] === numbers[j + 1]);
-    }
-    else if (sum > target) {
-      j--;
+      ans.push([numbers[left], numbers[right]]);
+      while (left < right && numbers[left] === numbers[left + 1]) {
+        left++;
+      }
+      while (left < right && numbers[right] === numbers[right - 1]) {
+        right--;
+      }
+      left++;
+      right--;
+    } else if (sum > target) {
+      right--;
     } else {
-      i++;
+      left++;
     }
   }
   return ans;
@@ -47,4 +48,3 @@ function twoSum(numbers, target) {
 
 const nums = [-1,0,1,2,-1,-4,-2,-3,3,0,4];
 console.log(threeSum(nums));
-console.log(twoSum([-4,0,0,1,2,3,4], 3))

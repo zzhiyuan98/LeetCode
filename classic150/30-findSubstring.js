fbbf9134d68f7ab78f7e7fa1b57e94ab84f45781
zzhiyuan98 <zzhiyuan@umich.edu>
2024-03-04 13:01:02 +0800

    WIP: classic 15 - find substring

---
+++ classic150/30-findSubstring.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+var findSubstring = function(s, words) {
+  const res = [];
+  const len = words.length * words[0].length;
+  let left = 0;
+  let right = left + len;
+  while (right < s.length) {
+
+  }
+  return res;
+};
